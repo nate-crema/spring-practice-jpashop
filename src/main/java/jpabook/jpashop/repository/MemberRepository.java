8e46005266269fe442a4bf78cb3370b02a5d1472
waffle <nate.crema@gmail.com>
2024-09-19 21:23:39 +0900

    fix: EntityManager field final keyword 추가

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -4,15 +4,19 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.PersistenceUnit;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository // : 아래 class를 Spring Bean으로 등록
+@RequiredArgsConstructor // : final 키워드가 붙은 필드들을 입력받는 생성자 자동추가
 public class MemberRepository {
 
-    @PersistenceContext // : Spring이 EntityManager를 생성하여 해당 `MemberRepository.em`에 자동으로 injection
-    private EntityManager em;
+//    @PersistenceContext // : Spring이 EntityManager를 생성하여 해당 `MemberRepository.em`에 자동으로 injection
+    @Autowired // : Autowired 내부적으로 EntityManager인 경우 @PersistenceContext인 것처럼 동작 (thanks to spring boot jpa)
+    private final EntityManager em;
 
     // +) EntityManagerFactory를 inject 받으려면
     // @PersistenceUnit
